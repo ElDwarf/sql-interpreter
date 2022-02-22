65329b1af2bc96004efba65b28bce143c3c0e32e
Pablo n Dalmasso <pablodalmasso@gmail.com>
2022-02-22 12:36:53 -0300

    Update

--- sql_interpreter/__init__.py
+++ sql_interpreter/__init__.py
@@ -1,19 +1,20 @@
 import re
 
-from sql_interpreter.sql import (SQL_REGEX_SELECT_WITH_WHERE,
+"""from sql_interpreter.sql import (SQL_REGEX_SELECT_WITH_WHERE,
                                  SQL_REGEX_SELECT_WITH_SUBQUERY,
                                  SQL_REGEX_SELECT)
+"""
 from sql_interpreter.sql.sql import SQL
 
 
 def new(sql_string: str) -> None:
-    sql_regexs = [SQL_REGEX_SELECT_WITH_SUBQUERY, SQL_REGEX_SELECT_WITH_WHERE,
+    """sql_regexs = [SQL_REGEX_SELECT_WITH_SUBQUERY, SQL_REGEX_SELECT_WITH_WHERE,
                   SQL_REGEX_SELECT]
     for regex in sql_regexs:
         patron = re.compile(regex, re.I)
         matcher = patron.search(sql_string)
         if matcher is not None:
-            break
+            break"""
     select = matcher.group('select')
     table = matcher.group('table')
     where = matcher.group('where')
--- sql_interpreter/sql/__init__.py
+++ sql_interpreter/sql/__init__.py
@@ -1,8 +1,17 @@
-SQL_REGEX_SELECT = "SELECT([ ]+)(?P<select>[A-Za-z0-9_\W*]*)([ ]+)FROM([ ]+)(?P<table>[A-Za-z0-9_\W*]*)"
+SQL_REGEX_SELECT = "SELECT([ ]+)(?P<select>[A-Za-z0-9_\W*]*)([ ]+)FROM"
 
-SQL_REGEX_SELECT_WITH_WHERE = "SELECT([ ]+)(?P<select>[A-Za-z0-9_\W*]*)([ ]+)FROM([ ]+)(?P<table>[A-Za-z0-9_\W*]*)([ ]+)WHERE([ ]+)(?P<where>[A-Za-z0-9_\W*]*)"
+SQL_REGEX_FROM = "FROM([ ]+)([/(]?)(?P<table>[A-Za-z0-9_\W*]*)([/)]?)(([ ]+)LEFT([ ]+)JOIN(.*)|([ ]+)INNER([ ]+)JOIN(.*)|([ ]+)RIGHT([ ]+)JOIN(.*)|([ ]+)WHERE(.*)|([ ]+)GROUP(.*)|([ ]+)ORDER(.*)|$)"
 
-SQL_REGEX_SELECT_WITH_SUBQUERY = "SELECT([ ]+)(?P<select>[A-Za-z0-9_\W*]*)([ ]+)FROM([ ]+)\((?P<table>[A-Za-z0-9_\W*]*)\)([ ]+)WHERE([ ]+)(?P<where>[A-Za-z0-9_\W*]*)"
+SQL_REGEX_JOIN = "(?P<join>LEFT([ ]+)JOIN(.*)|INNER([ ]+)JOIN(.*)|RIGHT([ ]+)JOIN(.*))([ ]+)WHERE(.*)|([ ]+)GROUP(.*)|([ ]+)ORDER(.*)|$"
+
+# En proceso no funciona todavia
+SQL_REGEX_WHERE = "WHERE[ ]+(?P<where>(.*))([ ]+)GROUP(.*)|([ ]+)ORDER(.*)|$"
+
+SQL_REGEX_GROUP = "GROUP(?P<group>(.*))([ ]+)ORDER(.*)|$"
+
+SQL_REGEX_ORDER = "ORDER(?P<oder>(.*))([ ]+)GROUP(.*)|$"
+
+
 
 
 SQL_REGEX_COLUMN = '(?P<column>[A-Za-z0-9_\W*]*)'
